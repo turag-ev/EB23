28dff49c9608363225add3add5a5aaccaf67079e
Niklas Weber <niklas.weber2@mailbox.tu-dresden.de>
2022-11-02 22:33:39 +0100

    Added general structure

---
+++ workspace/src/InternalMechanics/IMA_Interface/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+package_name = "TODO"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="TODO",
+    maintainer_email="TODO",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+)
---
+++ workspace/src/InternalMechanics/README.md
@@ -0,0 +1,10 @@
+# Internal Mechanics
+Working space for anything related to internal mechanics.
+
+## Content
+- [Actors](): API for communication with actors/ motors (ROS package)
+- [IMAM](): Internal Mechanics Actions Manager (ROS Node)
+- [IMA Interface](): Interface for Internal Mechanics Actions (Python Package)
+
+## Maintainers
+- Niklas
