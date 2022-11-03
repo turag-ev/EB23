5b54bab5af4343a9e649e7538dd97dff849ab88d
Niklas Weber <niklas.weber2@mailbox.tu-dresden.de>
2022-11-03 11:57:29 +0100

    added IMA_Interface class

--- workspace/src/InternalMechanics/IMA_Interface/IMA_Interface/interface.py
+++ workspace/src/InternalMechanics/IMA_Interface/IMA_Interface/interface.py
@@ -1,2 +1,13 @@
-class IMAI:
-    pass
+from abc import ABC, abstractmethod
+
+
+class IMA_Interface(ABC):
+    """
+    Interface for Internal Mechanics Actions.
+    """
+
+    used_actors: list[str]
+
+    @abstractmethod
+    def execute(self):
+        pass
