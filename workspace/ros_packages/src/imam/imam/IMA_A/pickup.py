71afe7c86209a4e0184c92ee942dc31c64da15d3
Niklas Weber <niklas.weber2@mailbox.tu-dresden.de>
2022-12-14 23:24:14 +0100

    added motor class mockup

--- workspace/ros_packages/src/imam/imam/IMA_A/pickup.py
+++ workspace/ros_packages/src/imam/imam/IMA_A/pickup.py
@@ -22,15 +22,42 @@ class PickUp(IMA):
         return register
 
     def execute(self, imam: object, goal_handle, **kwargs) -> bool:
-        for i in range(10):
+        for i in range(5):
             imam.log_info(f"Picking Up {i}")
             sleep(0.5)
 
-        sub_task1 = imam.loop.create_task(imam.clis.send_store_execute())
-        sub_task2 = imam.loop.create_task(imam.clis.send_drop_execute())
-        future_1 = asyncio.wait([sub_task1, sub_task2])
+        print("sending motor control 1")
+        feedback = 0
+        while (True):
+            if (feedback == 10):
+                break
+            else:
+                sleep(0.5)
+                feedback += 1
+        print("sending motor control 2")
+        while (True):
+            if (feedback == 18):
+                break
+            else:
+                sleep(0.5)
+                feedback += 1
+        
+        return True
+
+        
+        
+
+        
+
+        #sub_task1 = imam.loop.create_task(imam.clis.send_store_execute())
+        #sub_task2 = imam.loop.create_task(imam.clis.send_drop_execute())
+        #future_1 = asyncio.wait([sub_task1, sub_task2])
         # await future_1
 
-        sub_task3 = imam.loop.create_task(imam.clis.send_build_cake_execute())
-        future_2 = asyncio.wait([sub_task3])
+        #sub_task3 = imam.loop.create_task(imam.clis.send_build_cake_execute())
+        #future_2 = asyncio.wait([sub_task3])
         # await future_2
+
+        imam.clis.send_store_execute()
+
+        imam.clis.send_build_cake_execute()
---
+++ workspace/ros_packages/src/imam/imam/motor.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from time import sleep
+
+from imam import IMAM
+
+class Motor:
+    def __init__(self, name, imam: Node) -> None:
+        self.imam = imam
+        self.name = name
+        self.current_pos = 0
+        self.goal_pos_pub = self.imam.create_publisher(String, f"{name}_goal_pos", 20)
+        self.current_pos_sub = self.imam.create_subscription(String, f"{name}_current_pos", self.update_current_pos, 20)
+    
+    def update_current_pos(self, msg):
+        self.current_pos = int(msg.data)
+
+    def get_current_pos(self):
+        return self.current_pos
+    
+    def set_goal_pos(self, goal_pos: str):
+        msg = String()
+        msg.data = goal_pos
+        self.goal_pos_pub.publish(msg)
+
+
+if __name__ == "__main__":
+    motor1 = Motor("whoop", "sdsd")
+    motor2 = Motor("doop", "sdsd")
+
+    motor1.set_goal_pos("20")
+
+    while motor1.get_current_pos() < 5:
+        sleep(0.02)
+
+    motor2.set_goal_pos("30")
+
+    while motor2.get_current_pos() < 30:
+        sleep(0.02)
+
+
